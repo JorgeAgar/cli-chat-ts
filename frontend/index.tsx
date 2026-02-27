ddb4cc5dd8b1965bced6b39e62fb652534cafabc
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-02-26 21:10:33 -0500

    Revert "use opentui react bindings"
    This reverts commit bdc55b586ae6b86cfc199316767c3975bfd7731f.

--- frontend/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createCliRenderer } from "@opentui/core"
-import { createRoot } from "@opentui/react"
-
-function App() {
-  return <text>Hello, world!</text>
-}
-
-const renderer = await createCliRenderer()
-createRoot(renderer).render(<App />)

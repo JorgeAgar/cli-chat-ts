35064f2d383fe98d6e00bf8925a1bb1f1db9682f
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-04-02 23:27:25 -0500

    backend/add zod schema based on schema

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -1,4 +1,6 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { createInsertSchema } from "drizzle-zod";
+import z from "zod";
 
 export const user = sqliteTable('users', {
   id: text().primaryKey(),
@@ -7,9 +9,16 @@ export const user = sqliteTable('users', {
   password: text().notNull(),
 });
 
+export const userInsertSchema = createInsertSchema(user);
+
 export const room = sqliteTable('rooms', {
   id: text().primaryKey(),
   name: text().notNull(),
   password: text(),
   owner: text().notNull().references(() => user.id),
 });
+
+const roomInsertSchema = createInsertSchema(room);
+
+export type insertUser = z.infer<typeof userInsertSchema>;
+export type insertRoom = z.infer<typeof roomInsertSchema>;
